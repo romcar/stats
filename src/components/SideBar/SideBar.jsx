87068998d11f2203b830e5ba6842dd75d0432f30
romcar <rommel.carrasquilla@gmail.com>
2018-08-24 20:25:23 -0400

    Start input component

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
+import profilePicPlaceholder from '../../assets/profile_pic_placeholder.jpg';
+
 import StyledSideBar from '../../styledComponents/StyledSideBar/StyledSideBar';
-export default () => {
-  return (
-    <StyledSideBar>Sidebar</StyledSideBar>
-  )
-}
+import StyledProfilePic from '../../styledComponents/StyledProfilePic/StyledProfilePic';
+export default class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+
+    this.toggleSideBar = this.toggleSideBar.bind(this);
+  };
+
+  toggleSideBar() {
+    const { isOpen } = this.state;
+    this.setState({ isOpen: !isOpen });
+  }
+
+  profilePicTransition() {
+    let { isOpen } = this.state;
+    if (isOpen) {
+      return (
+        <StyledProfilePic open={!isOpen} src={profilePicPlaceholder}></StyledProfilePic>
+      );
+    } else {
+      return (
+        <StyledProfilePic open={!isOpen} src={profilePicPlaceholder}></StyledProfilePic>
+      );
+    }
+  }
+  render() {
+    return (
+      <StyledSideBar onMouseEnter={() => (this.toggleSideBar())} onMouseLeave={() => (this.toggleSideBar())} >
+        {this.profilePicTransition()}
+      </StyledSideBar>
+    );
+  };
+};
+
+
+
